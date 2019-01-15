0346b44cbe5e7425579dfb2d2957bc8f778134f3
João Vortmann <jvortmann@gmail.com>
2019-01-15 18:38:04 -0200

    doc: Update setup description

--- setup_ruby/Readme.mk
+++ setup_ruby/Readme.mk
@@ -1,7 +1,7 @@
 After the problem folder is alredy created at `../dojos/{problem}`
 
 Copy code to problem folder: `cp -r . ../dojos/{problem}/ruby`
-Go to top level: `cd ..`
+Go to problem folder: `cd ../dojos/{problem}/ruby`
 
 Install ruby 2.6: `rbenv install 2.6.0`
 Install bundler: `gem install bundler`
