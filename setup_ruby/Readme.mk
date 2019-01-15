e0dbe613276c9eecaee4aec4f8f969f9945fb036
João Vortmann <jvortmann@gmail.com>
2019-01-15 18:37:23 -0200

    chore: Add ruby setup bootstrap

---
+++ setup_ruby/Readme.mk
@@ -0,0 +1,9 @@
+After the problem folder is alredy created at `../dojos/{problem}`
+
+Copy code to problem folder: `cp -r . ../dojos/{problem}/ruby`
+Go to top level: `cd ..`
+
+Install ruby 2.6: `rbenv install 2.6.0`
+Install bundler: `gem install bundler`
+
+Install dependencies: `bundle`
