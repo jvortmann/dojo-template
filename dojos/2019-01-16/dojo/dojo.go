dee3ba9d775d1218786460e52a2908a1286f4bbf
cristiano.matte <cristiano.matte@corp.globo.com>
2019-01-16 17:48:06 -0200

    feat: 2019-01-16 code

--- dojos/2019-01-16/dojo/dojo.go
+++ dojos/2019-01-16/dojo/dojo.go
@@ -1,5 +1,19 @@
 package dojo
 
-func dojo() string {
-	return "dojo"
+func getRotationOffset(array []int) int {
+	for i := 0; i < len(array)-1; i++ {
+		if array[i] > array[i+1] {
+			return i + 1
+		}
+	}
+	return 0
+}
+
+func binarySearch(array []int, offset int, element int) int {
+	return 0
+}
+
+func getElementIndex(array []int, element int) int {
+
+	return -1
 }
--- dojos/2019-01-16/dojo/dojo_test.go
+++ dojos/2019-01-16/dojo/dojo_test.go
@@ -6,7 +6,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDojo(t *testing.T) {
-	result := dojo()
-	assert.Equal(t, result, "dojo")
+func TestGetElementIndexReturnsMinusOneWhenMissingElement(t *testing.T) {
+	result := getElementIndex([]int{3, 1, 2}, 4)
+	assert.Equal(t, result, -1)
+}
+
+// func TestGetElementIndexReturnsTheIndexOfTheElementWhenFound(t *testing.T) {
+// 	result := getElementIndex([]int{3, 1, 2}, 1)
+// 	assert.Equal(t, result, 1)
+// }
+
+func TestGetRotationOffsetReturnsZeroWhenNoRotation(t *testing.T) {
+	result := getRotationOffset([]int{1, 2, 3})
+	assert.Equal(t, result, 0)
+}
+
+func TestGetRotationOffsetReturnsCorrectOffsetWhenRotated(t *testing.T) {
+	assert.Equal(t, getRotationOffset([]int{4, 1, 2, 3}), 1)
+	assert.Equal(t, getRotationOffset([]int{3, 4, 1, 2}), 2)
+	assert.Equal(t, getRotationOffset([]int{2, 3, 4, 1}), 3)
+}
+
+func TestBinarySearchReturnsOneForOrderedArray(t *testing.T) {
+	assert.Equal(t, binarySearch([]int{1, 2, 3}, 0, 1), 0)
 }
