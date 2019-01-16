ef67ebde1724023d3c988baa624f0ef39de6e248
João Vortmann <jvortmann@gmail.com>
2019-01-16 19:36:29 -0200

    feat: Add setup script for ruby problem

---
+++ setup_ruby/setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -ne 1 ]
+then
+  echo 'Usage: ./setup.sh "Nome do Problema"'
+  exit 1
+fi
+
+problem="$1"
+base_path="../dojos"
+language="ruby"
+
+folder="$base_path/$(echo "$problem" | tr '[:upper:]' '[:lower:]'| tr ' ' '_')/$language"
+
+if [ -d "$folder" ]; then
+  echo ">>> Problem folder for '$problem' already exist at '$folder'"
+else
+  echo ">>> Create problem folder for '$problem' at '$folder'"
+  [ ! -d "$folder" ] && mkdir -p "$folder" && cp -r "$(pwd)/" "$folder/"
+
+  echo ">>> Set date"
+  sed "s/\[DATA\]/$(date "+%Y-%m-%d %H:%M:%S")/g" "README.md" > "$folder/README_tmp.md"
+
+  echo ">>> Set Problem"
+  sed "s/\\[PROBLEM\\]/$problem/g" "$folder/README_tmp.md" > "$folder/README.md"
+
+  echo ">>> Set Ruby Version"
+  sed "s/\[RUBY_VERSION\]/$(cat .ruby-version)/g" "$folder/README.md" > "$folder/README_tmp.md"
+
+  mv "$folder/README_tmp.md" "$folder/README.md"
+
+  echo ">>> Remove setup script"
+  [ -e "$folder/setup.sh" ] && rm "$folder/setup.sh"
+fi
+
+echo ""
+echo ">>> DONE"
