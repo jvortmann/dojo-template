60ec82812f42c46312179b4deee66b3cdfe38e7d
Igor Cescon de Moura <igor.moura@corp.globo.com>
2019-02-11 14:33:35 -0200

    style: convert all tabs to spaces

--- setup/rust/template/src/main.rs
+++ setup/rust/template/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-	println!("Welcome do the Dojo");
+    println!("Welcome do the Dojo");
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn TestMain() {
-		assert_eq!(1,1);	
-	}
+        assert_eq!(1,1);
+    }
 }
