a31815fdad3ce743902bd613fbb81728e88c0ae8
Igor Cescon de Moura <igor.moura@corp.globo.com>
2019-02-14 18:54:44 -0200

    chore: ignore rust auto generated files

--- dojos/numeros-romanos/rust/target/rls/debug/deps/template-570ed077aa347ca0.d
+++
@@ -1,5 +0,0 @@
-/Users/igor.moura/globo.com/poa/dojo-poa/dojos/numeros-romanos/rust/target/rls/debug/deps/template-570ed077aa347ca0.rmeta: src/main.rs
-
-/Users/igor.moura/globo.com/poa/dojo-poa/dojos/numeros-romanos/rust/target/rls/debug/deps/template-570ed077aa347ca0.d: src/main.rs
-
-src/main.rs:
