c036071099ab826e30d12e883260d9669bf8d711
Rafael Eyng <rafaeleyng@gmail.com>
2019-03-21 17:02:48 -0300

    feat: add 2019-03-21 dojo solution

--- dojos/2019-03-21/go/README.md
+++ dojos/2019-03-21/go/README.md
@@ -1,27 +1,32 @@
-# DOJO 2019-01-31 16:12:26
+# DOJO 2019-03-21 15:30:00
 
 ## Howto
 
 ### How to run the tests:
   - `make test`
 
-## Problema: [Escrevendo no Celular](http://dojopuzzles.com/problemas/exibe/escrevendo-no-celular/)
+## Problema: [Amicable numbers](https://projecteuler.net/problem=21)
 
 
 ## Retro
 
 #### O que aprendemos hoje?
 
-- Manipulação de strings em Go
-- Mapas em Go
-- Linguagem Go
+- o que é amicable number
+- map em Go
+- `print` (não só `fmt.Print`)
 
 #### O que foi bom
 
-- Tempo ideal para resolução do problema
-- Contribuição dos integrantes
-- Problema legal
+- o problema foi legal
+- bem dimensionado para o tempo
+- encontramos uma solução "esperta" que conseguiu resolver em uma só passada
+- testes
+- respeito ao formato, piloto e co-piloto falando mais e o resto escutando e tirando dúvidas
+- o horário (15:30) contribuiu para que algumas pessoas pudessem participar
 
 #### O que podemos melhorar
 
-- Utilizar TDD e babysteps melhor
+- o horário (15:30) contribuiu para que algumas pessoas NÃO pudessem participar. Se houverem 2 dojos na semana, podemos fazer um mais cedo e um mais tarde.
+- o pessoal largou
+- tornar a organização mais simples, talvez fazendo via Slack
--- dojos/2019-03-21/go/main_test.go
+++ dojos/2019-03-21/go/main_test.go
@@ -4,11 +4,46 @@ import (
 	"testing"
 )
 
-func TestReturn1(t *testing.T) {
-	result := return1()
-	expected := 1
-
-	if result != expected {
-		t.Errorf("Got %d, expected %d", result, expected)
+func compareNumbers(actual, expected int, t *testing.T) {
+	if actual != expected {
+		t.Errorf("Got %v, expected %v", actual, expected)
 	}
+}
+
+func compareSlices(actual, expected []int, t *testing.T) {
+	compareNumbers(len(actual), len(expected), t)
+
+	for i := range actual {
+		compareNumbers(actual[i], expected[i], t)
+	}
+}
+
+func TestGetDivisorsWith6(t *testing.T) {
+	actual := getDivisors(6)
+	expected := []int{1, 2, 3}
+	compareSlices(actual, expected, t)
+}
+
+func TestGetDivisorsWith100(t *testing.T) {
+	actual := getDivisors(100)
+	expected := []int{1, 2, 4, 5, 10, 20, 25, 50}
+	compareSlices(actual, expected, t)
+}
+
+func TestSumNumbers(t *testing.T) {
+	actual := sumNumbers([]int{1, 2, 3, 4, 5})
+	expected := 15
+	compareNumbers(actual, expected, t)
+}
+
+func TestSumAmicableNumbers(t *testing.T) {
+	actual := sumAmicableNumbers(300)
+	expected := 220 + 284
+	compareNumbers(actual, expected, t)
+}
+
+func TestSumAmicableNumbersFor10000(t *testing.T) {
+	actual := sumAmicableNumbers(10000)
+	expected := 220 + 284 + 1184 + 1210 + 2620 + 2924 + 5020 + 5564 + 6232 + 6368
+	compareNumbers(actual, expected, t)
 }
