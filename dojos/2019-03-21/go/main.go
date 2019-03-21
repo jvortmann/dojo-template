32ebe8110c5a571391b9cfa69136a51556ac2e65
Rafael Eyng <rafaeleyng@gmail.com>
2019-03-21 17:02:48 -0300

    feat: add 2019-03-21 dojo solution

--- dojos/2019-03-21/go/main.go
+++ dojos/2019-03-21/go/main.go
@@ -1,8 +1,49 @@
 package main
 
+import "fmt"
+
 func main() {
+	finalSum := sumAmicableNumbers(10000)
+	fmt.Printf("And the final sum is... %d\n", finalSum)
 }
 
-func return1() int {
-	return 1
+func getDivisors(number int) []int {
+	result := []int{}
+
+	for i := 1; i < number; i++ {
+		if number%i == 0 {
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
+
+func sumNumbers(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func sumAmicableNumbers(limit int) int {
+	memoizedResults := make(map[int]int)
+	amicableNumbers := make([]int, 0)
+
+	for i := 1; i < limit; i++ {
+		divisors := getDivisors(i)
+		sum := sumNumbers(divisors)
+		memoizedResults[i] = sum
+		amicableSum, exists := memoizedResults[sum]
+		if !exists || i == sum {
+			continue
+		}
+
+		if amicableSum == i {
+			amicableNumbers = append(amicableNumbers, sum, i)
+		}
+	}
+
+	return sumNumbers(amicableNumbers)
 }
