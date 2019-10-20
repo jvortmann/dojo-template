a9857da6922a70e36605174fe877a483fde6b2ed
João Vortmann <jvortmann@gmail.com>
2019-10-20 09:18:23 -0400

    refactor: Extract help and description tasks

---
+++ setup/prerequisites.mk
@@ -0,0 +1,14 @@
+context_file = $(word 1,$(abspath $(MAKEFILE_LIST)))
+context_name = $(shell echo $(dir $(context_file)) | sed -E "s/^.*dojos\/(.*)\/(.*)\/$$/\2/")
+
+.PHONY: help description
+.DEFAULT_GOAL := help
+
+## help: show this list
+help: description
+	@sed -nE 's/^## (.*): (.*)$$/"\1" "\2"/p' $(context_file) | xargs printf "\033[34m  %-20s\033[0m%s\n"
+	@echo
+
+## description: show description
+description:
+	@sed -nE 's/^##>> (.*)$$/"\1"/p' $(context_file) | xargs printf "\033[37m%-20s\033[0m  \033[33m%-20s\033[0m%s\n" "[$(context_name)]"
