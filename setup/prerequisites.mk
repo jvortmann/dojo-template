c74ec9bd377956587a20682bccc14932bfe40e94
João Vortmann <jvortmann@gmail.com>
2019-10-20 13:15:29 -0400

    refactor: Extract an simplify docker command to run tasks

--- setup/prerequisites.mk
+++ setup/prerequisites.mk
@@ -4,6 +4,8 @@ context_name = $(shell echo $(dir $(context_file)) | sed -E "s/^.*dojos\/(.*)\/(
 .PHONY: help description
 .DEFAULT_GOAL := help
 
+docker = docker run --rm -it -v ${PWD}:/dojo/problem dojo/$(1):$(2) $(3)
+
 ## help: show this list
 help: description
 	@sed -nE 's/^## (.*): (.*)$$/"\1" "\2"/p' $(context_file) | xargs printf "\033[34m  %-20s\033[0m%s\n"
