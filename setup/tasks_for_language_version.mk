c377e7e752b8bc9dd83659d232cc964dd16f8f32
João Vortmann <jvortmann@gmail.com>
2019-10-20 15:02:45 -0400

    refactor: Extract language version file generation

--- setup/tasks_for_language_version.mk
+++ setup/tasks_for_language_version.mk
@@ -5,3 +5,6 @@ badge: ${folder}/README.md
 	@sed -i .bkp -e "s/\\[LANGUAGE\\]/${language}/g" -e "s/\\[VERSION\\]/${version}/g" "${folder}/README.md"
 	@rm ${folder}/README.md.bkp
 
+${folder}/.${language}-version: ${folder}
+	@echo "Setting ${language} version file"
+	@echo ${version} > ${folder}/.${language}-version
