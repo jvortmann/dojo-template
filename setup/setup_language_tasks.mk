52bce10733eb0aad2ee07a6f498f7a11e63b67ae
João Vortmann <jvortmann@pm.me>
2021-05-07 18:55:06 -0300

    Build images dynamically if they do not exist
    --
    
    Github-issue: #2
    Changelog: feature

--- setup/setup_language_tasks.mk
+++ setup/setup_language_tasks.mk
@@ -39,4 +39,4 @@ ${folder}/.tool-versions: ${folder}
 
 ${folder}/Makefile: ${folder}
 	@echo "Setting Makefile"
-	@sed "s/\\[VERSION\\]/${version}/g" "${setup_base}/template/Makefile" > ${folder}/Makefile
+	@sed -e "s/\\[VERSION\\]/${version}/g" -e "s#\\[SETUP_BASE\\]#${setup_base}#g" "${setup_base}/template/Makefile" > ${folder}/Makefile
